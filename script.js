9a8bcebb8122994d1ad33c7ffb0fb6295166d5d6
mtaudon <88857883+mtaudon@users.noreply.github.com>
2021-09-08 12:37:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// TYPED
+var typed = new Typed('.typed', {
+    strings: ["Bonjour, bienvenue sur mon site WEB, qui a pour vocation d'être mon cv . Ce site est donc encore en élaboration."],
+    typeSpeed: 20,
+    
+    
+  });
