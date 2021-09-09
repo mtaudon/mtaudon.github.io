6f38b0063fc28186b1380f1c69327c35ab2f6a47
mtaudon <88857883+mtaudon@users.noreply.github.com>
2021-09-09 22:13:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ var typed = new Typed('.typed', {
     
     
   });
+
+  //COMPTEUR
+
+  let compteur = 0;
+
+  $(window).scroll(function(){ 
+
+    const top = $('.counter').offset().top -
+    window.innerHeight;
+
+    if (compteur == 0 && $(window).scrollTop() > top) {
+      $('.counter-value').search(function() {
+        let $this =  $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: this.text()
+        }).animate({
+          countNum : countTo 
+        },
+        {
+          duration: 10000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function(){
+            $this.text(this.countNum);
+          }
+        });        
+      });
+      compteur = 1;
+    }
+});
